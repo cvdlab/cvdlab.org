eca4108f12023c393b3e6e26df3417fcab54c67d
Federico Spini <federico.spini@gmail.com>
2014-05-30 22:44:06 +0200

    added favicon

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ var PORT = 9011;
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
+var app = express();
+app.use(favicon(__dirname + '/public/favicon.ico'));
 var express = require('express');
 var app = express();
 
 app.enable('trust proxy');
 
 app.use(logger('dev'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser());
 app.use(methodOverride());
