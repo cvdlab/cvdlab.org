25333d3272d6d7df04c80ca8b710c71ae2e9d8b6
enricomarino <enrico.marino@email.com>
2017-02-15 17:13:55 +0100

    .

--- scripts/ga.js
+++ scripts/ga.js
@@ -1,3 +1,13 @@
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/58a466dd66272e0ab02d1d5d/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+
 (function(i, s, o, g, r, a, m) {
     i['GoogleAnalyticsObject'] = r;
     i[r] = i[r] || function() {
