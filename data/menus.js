1c6c36a94399ce7d95fb6b398202da77955c45ff
enricomarino <enrico.marino@email.com>
2017-03-14 11:44:11 +0100

    .

---
+++ data/menus.js
@@ -0,0 +1,32 @@
+Pantarei.menus = [
+  {
+    "name": "main",
+    "label": "CVDLAB",
+    "items": [
+      {
+        "label": "about",
+        "page": "page-0101"
+      },
+      {
+        "label": "people",
+        "page": "page-0201"
+      },
+      {
+        "label": "projects",
+        "page": "page-0301"
+      },
+      {
+        "label": "theses",
+        "page": "page-0401"
+      },
+      {
+        "label": "courses",
+        "page": "page-0501"
+      },
+      {
+        "label": "contacts",
+        "page": "page-0601"
+      }
+    ]
+  }
+]
--- data/pages.js
+++ data/pages.js
@@ -1,48 +1,66 @@
-Pantarei.pages = {
-  "page-0000": {
+Pantarei.pages = [
+  {
+    "id": "0000",
+    "name": "page-0000",
     "layout": "layout-page",
     "link": "/404",
     "title": "Pagina non trovata",
     "notfound": true
   },
-  "page-0001": {
+  {
+    "id": "0001",
+    "name": "page-0001",
     "layout": "layout-land",
     "link": "/",
     "title": "CVDLAB"
   },
-  "page-0002": {
+  {
+    "id": "0002",
+    "name": "page-0002",
     "layout": "layout-page",
     "link": "/cookie_policy",
     "title": "Cookie policy"
   },
-  "page-0101": {
+  {
+    "id": "0101",
+    "name": "page-0101",
     "layout": "layout-page",
     "link": "/about",
     "title": "About"
   },
-  "page-0201": {
+  {
+    "id": "0201",
+    "name": "page-0201",
     "layout": "layout-page",
     "link": "/people",
     "title": "People"
   },
-  "page-0301": {
+  {
+    "id": "0301",
+    "name": "page-0301",
     "layout": "layout-page",
     "link": "/projects",
     "title": "Projects"
   },
-  "page-0401": {
+  {
+    "id": "0401",
+    "name": "page-0401",
     "layout": "layout-page",
     "link": "/theses",
     "title": "Theses"
   },
-  "page-0501": {
+  {
+    "id": "0501",
+    "name": "page-0501",
     "layout": "layout-page",
     "link": "/courses",
     "title": "Courses"
   },
-  "page-0601": {
+  {
+    "id": "0601",
+    "name": "page-0601",
     "layout": "layout-page",
     "link": "/contacts",
     "title": "Contacts"
   }
-}
+]
