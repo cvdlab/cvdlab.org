9fd21aca9d6a8f50e922e166aca4125e00130182
enricomarino <enrico.marino@email.com>
2017-04-11 17:10:49 +0200

    .

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'cvdlab-cache-0.0.1'
+const CACHE_NAME = 'cvdlab-cache-0.0.2'
 
 const files = [
   '/data/alumni.js',
@@ -34,52 +34,59 @@ const files = [
   '/scripts/tawk.js'
 ]
 
-self.addEventListener('install', function (event) {
-  console.log('[ServiceWorker] Install')
-  event.waitUntil(precache())
-})
+self.addEventListener('activate', function(event) {
 
-self.addEventListener('activate', function (event) {
-  console.log('[ServiceWorker] Activate')
-  event.waitUntil(clear_caches())
-  return self.clients.claim()
-})
+  var cacheWhitelist = [CACHE_NAME];
 
-self.addEventListener('fetch', function (event) {
-  console.log('[ServiceWorker] Fetch', event.request.url)
-  event.respondWith(from_cache(event.request))
-  event.waitUntil(update(event.request))
-})
-
-function precache () {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.addAll(files)
-  })
-}
-
-function clear_caches () {
-  return caches.keys().then((keys) => {
-    return Promise.all(keys.map((key) => {
-      if (key !== CACHE) {
-        console.log('[ServiceWorker] Removing old cache', key)
-        return caches.delete(key)
-      }
-    }))
-  })
-}
-
-function from_cache (request) {
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response);
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
     })
-  })
-}
+  );
+});
 
-function update (request) {
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response)
-    })
-  })
-}
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        // IMPORTANT: Clone the request. A request is a stream and
+        // can only be consumed once. Since we are consuming this
+        // once by cache and once by the browser for fetch, we need
+        // to clone the response.
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
